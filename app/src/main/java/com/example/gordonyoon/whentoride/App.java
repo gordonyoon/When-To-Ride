3bac60eb40a190149856b2d72af54caede8c6eef
Gordon Yoon <gordonyoon@gmail.com>
2015-09-25 17:43:44 -0700

    Integrate Stetho

---
+++ app/src/main/java/com/example/gordonyoon/whentoride/App.java
@@ -0,0 +1,20 @@
+package com.example.gordonyoon.whentoride;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.okhttp.StethoInterceptor;
+import com.squareup.okhttp.OkHttpClient;
+
+
+public class App extends Application {
+
+    public static OkHttpClient okHttpClient = new OkHttpClient();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+        okHttpClient.networkInterceptors().add(new StethoInterceptor());
+    }
+}
--- app/src/main/java/com/example/gordonyoon/whentoride/uberapi/UberAuthTokenClient.java
+++ app/src/main/java/com/example/gordonyoon/whentoride/uberapi/UberAuthTokenClient.java
@@ -1,9 +1,12 @@
 package com.example.gordonyoon.whentoride.uberapi;
 
 
+import android.content.Context;
 import android.net.Uri;
 
+import com.example.gordonyoon.whentoride.App;
 import com.example.gordonyoon.whentoride.models.User;
+import com.squareup.okhttp.OkHttpClient;
 
 import retrofit.Call;
 import retrofit.GsonConverterFactory;
@@ -17,6 +20,7 @@ public class UberAuthTokenClient {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://login.uber.com")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(App.okHttpClient)
                 .build();
 
         return retrofit.create(UberAuthTokenService.class);
