856a34f9543ea8c2ce4cbf365369a24eddc79bc8
Gordon Yoon <gordonyoon@gmail.com>
2015-09-30 09:02:53 -0700

    Add the My Location Layer

--- app/src/main/java/com/example/gordonyoon/whentoride/EditFavoriteActivity.java
+++ app/src/main/java/com/example/gordonyoon/whentoride/EditFavoriteActivity.java
@@ -107,5 +107,6 @@ public class EditFavoriteActivity extends FragmentActivity {
      */
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.setMyLocationEnabled(true);
     }
 }
