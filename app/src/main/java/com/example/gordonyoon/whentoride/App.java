91a566c94f656bb7e7a6cd19301b6be96a845b25
Gordon Yoon <gordonyoon@gmail.com>
2015-10-03 12:40:44 -0700

    Add LeakCanary

--- app/src/main/java/com/example/gordonyoon/whentoride/App.java
+++ app/src/main/java/com/example/gordonyoon/whentoride/App.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.stetho.Stetho;
 import com.facebook.stetho.okhttp.StethoInterceptor;
+import com.squareup.leakcanary.LeakCanary;
 import com.squareup.okhttp.OkHttpClient;
 
 
@@ -16,5 +17,7 @@ public class App extends Application {
         super.onCreate();
         Stetho.initializeWithDefaults(this);
         okHttpClient.networkInterceptors().add(new StethoInterceptor());
+
+        LeakCanary.install(this);
     }
 }
