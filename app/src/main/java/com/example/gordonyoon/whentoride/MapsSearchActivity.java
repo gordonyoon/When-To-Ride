fc257e10b4e02c3a3b4a7dc0747afdf5dfbb58ea
Gordon Yoon <gordonyoon@gmail.com>
2015-10-03 23:02:31 -0700

    Create RecyclerView for search results

--- app/src/main/java/com/example/gordonyoon/whentoride/MapsSearchActivity.java
+++ app/src/main/java/com/example/gordonyoon/whentoride/MapsSearchActivity.java
@@ -4,9 +4,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MapsSearchActivity extends AppCompatActivity {
+    @Bind(R.id.search_results) RecyclerView mRecyclerView;
 
     public static void start(Context context) {
         Intent intent = new Intent(context, MapsSearchActivity.class);
@@ -17,10 +29,54 @@ public class MapsSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_search);
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        SearchAdapter adapter = new SearchAdapter(new ArrayList<>());
+        mRecyclerView.setAdapter(adapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @Bind(R.id.list_item_address) TextView mAddress;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(itemView);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    class SearchAdapter extends RecyclerView.Adapter<ViewHolder> {
+        private List<String> mAddresses;
+
+        public SearchAdapter(List<String> addresses) {
+            mAddresses = addresses;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.list_item_address, parent, false);
+            final ViewHolder vh = new ViewHolder(view);
+            view.setOnClickListener(vh);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.mAddress.setText(mAddresses.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mAddresses.size();
+        }
+    }
 }
